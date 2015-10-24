66ce7acb110c5b94d39635b258c052e6b368fc50
Michael Sogos <michael.sogos@gurustudioweb.it>
2015-10-24 17:22:04 +0200

    -

--- test/_references.js
+++ test/_references.js
@@ -1,6 +1,3 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="server.js" />
 /// <reference path="test.js" />
-/// <reference path="../src/hash-router.js" />
-/// <reference path="node_modules/chai/chai.js" />
-/// <reference path="node_modules/mocha/mocha.js" />
