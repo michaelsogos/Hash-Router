14bb8417481edee5cea7d86b9dd21c07a83375b2
Michael Sogos <michael.sogos@gurustudioweb.it>
2016-10-01 14:05:31 +0200

    Version 1.2.4

---
+++ test/_references.js
@@ -0,0 +1,3 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="server.js" />
+/// <reference path="test.js" />
---
+++ test/test.js
@@ -0,0 +1,14 @@
+﻿describe('Hash-Router', function () {
+	it('Global Definition', function () {
+		chai.assert.ok(Router !== null, "The [Router] object is defined globally!");
+	});
+	
+	it('Init', function () {
+		window.Router.init(function (route) {
+			console.info("Route changed to: " + route);
+		}, function (tokens) {
+			console.info("Route faild with tokens: " + tokens);
+		});
+	});
+})
+
